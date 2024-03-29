69a61837a508d526acdc038365d0b3e5115369a8
PenSul <jackychiang04@gmail.com>
2024-03-29 23:18:52 +0800

    Update

---
+++ game.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Game")
+screen = pygame.display.set_mode((800, 600))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
