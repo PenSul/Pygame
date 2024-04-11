8cfd3477b9bef9c2a97d368dbfaf8687ba51176d
PenSul <jackychiang04@gmail.com>
2024-04-11 09:56:23 +0800

    Update

--- scripts/menubutton.py
+++ scripts/menubutton.py
@@ -3,15 +3,26 @@ import os
 import sys
 
 # Scripts will be loaded here
-from scripts.utils import load_image
+from scripts.utils import load_image, outline
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 board_path = os.path.join(current_dir, "board.py")
 
+# Game setting and variable
+screen_width = 1000
+screen_height = 600
+
 # Button class
 class Button:
 	def __init__(self, x, y, image, scale, gameStateManager):
+		# Initialize pygame
 		pygame.init()
+		# Set the game screen size
+		self.screen = pygame.display.set_mode((screen_width, screen_height))
+		# Set the game display size
+		self.display = pygame.Surface((screen_width / 2, screen_height / 2))
+
+		# Button Variables
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -36,6 +47,7 @@ class Button:
 		# Check if the mouse is over the button and if it is clicked
 		if self.rect.collidepoint(pos):
 			self.image = self.assets['startbtnhover']  # Change the button image to the hover state
+			outline(self.image, (self.rect.topleft))  # Outline the button image with white line when hover
 			if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:  # Check if the left mouse button is clicked
 				self.clicked = True  # Set the clicked flag to True
 				action = True  # Set the action variable to True
--- scripts/utils.py
+++ scripts/utils.py
@@ -3,6 +3,15 @@ import os
 
 pygame.init()
 
+# Game setting and variable
+screen_width = 992
+screen_height = 600
+
+# Set the game screen size
+screen = pygame.display.set_mode((screen_width, screen_height))
+# Set the game display size
+display = pygame.Surface((screen_width / 2, screen_height / 2))
+
 BASE_IMG_PATH = 'assets/images'
 
 # Load image path
@@ -17,3 +26,17 @@ def load_images(path):
     for images_name in os.listdir(BASE_IMG_PATH + path):
         images.append(load_image(path + '/' + images_name))
     return images
+
+def outline(img, loc):
+    mask = pygame.mask.from_surface(img)
+    mask_outline = mask.outline()
+    mask_surf = pygame.Surface(img.get_size())
+    for pixel in mask_outline:
+        mask_surf.set_at(pixel, (255, 255, 255))
+    mask_surf.set_colorkey((0, 0, 0))
+    screen.blit(mask_surf, (loc[0]-1, loc[1]))
+    screen.blit(mask_surf, (loc[0]+1, loc[1]))
+    screen.blit(mask_surf, (loc[0], loc[1]-1))
+    screen.blit(mask_surf, (loc[0], loc[1]+1))
+
+    
